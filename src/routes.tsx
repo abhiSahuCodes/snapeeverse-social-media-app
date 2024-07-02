6d1250acd1c9c11d902bbe2cf749bac307bf1e1e
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-02 20:51:53 +0530

    Add router

---
+++ src/routes.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home";
+import Error from "./pages/error";
+import CreatePost from "./pages/post";
+import Profile from "./pages/profile";
+import MyPhotos from "./pages/photos";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+
+export const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/post",
+    element: <CreatePost />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/myphotos",
+    element: <MyPhotos />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    errorElement: <Error />,
+  },
+]);
+
+export default router;
