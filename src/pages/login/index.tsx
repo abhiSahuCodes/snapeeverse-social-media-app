5060e1d3aa3d790ac6bcf862cfbd3b73ef520b08
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-03 19:38:54 +0530

    Add login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,128 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Icons } from "@/components/ui/icons";
+import { Input } from "@/components/ui/input";
+import { useUserAuth } from "@/context/userAuthContext";
+import { UserLogIn } from "@/types";
+import { Label } from "@radix-ui/react-label";
 import * as React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 interface ILoginProps {}
+const initialValue: UserLogIn = {
+  email: "",
+  password: "",
+};
 
-const Login: React.FunctionComponent<ILoginProps> = (props) => {
-  return <div>Login</div>;
+const Login: React.FunctionComponent<ILoginProps> = () => {
+  const { googleSignIn, logIn } = useUserAuth();
+
+  const navigate = useNavigate();
+  
+  const [userLogInInfo, setuserLogInInfo] =
+    React.useState<UserLogIn>(initialValue);
+
+  const handleGoogleSignIn = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    try {
+      await googleSignIn();
+      navigate("/");
+    } catch (error) {
+      console.log("Error : ", error);
+    }
+  };
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      console.log("The user info is : ", userLogInInfo);
+      await logIn(userLogInInfo.email, userLogInInfo.password);
+      navigate("/");
+    } catch (error) {
+      console.log("Error : ", error);
+    }
+  };
+  return (
+    <div className="bg-slate-800 w-full h-screen flex flex-col justify-center items-center">
+      <div className="max-w-sm rounded-xl border bg-card text-card-foreground shadow-sm">
+        <Card>
+          <form onSubmit={handleSubmit}>
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-2xl text-center mb-4">
+                PhotoGram
+              </CardTitle>
+              <CardDescription>
+                Enter your email below to create your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid">
+                <Button variant="outline" onClick={handleGoogleSignIn}>
+                  <Icons.google className="mr-2 h-4 w-4" />
+                  Google
+                </Button>
+              </div>
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground">
+                    Or
+                  </span>
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email address</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="dipesh@example.com"
+                  value={userLogInInfo.email}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setuserLogInInfo({
+                      ...userLogInInfo,
+                      email: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  value={userLogInInfo.password}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setuserLogInInfo({
+                      ...userLogInInfo,
+                      password: e.target.value,
+                    })
+                  }
+                />
+              </div>
+            </CardContent>
+            <CardFooter className="flex flex-col">
+              <Button className="w-full" type="submit">
+                Login
+              </Button>
+              <p className="mt-3 text-sm text-center">
+                Don't have an account ? <Link to="/signup">Sign up</Link>
+              </p>
+            </CardFooter>
+          </form>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
