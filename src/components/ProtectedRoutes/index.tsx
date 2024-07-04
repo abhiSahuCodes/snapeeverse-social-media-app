1be651ee24f97bf9089cb10f1ff010aebed89d82
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-04 23:07:39 +0530

    Add refresh logout fix

--- src/components/ProtectedRoutes/index.tsx
+++ src/components/ProtectedRoutes/index.tsx
@@ -1,15 +1,20 @@
+import { getAuth } from "firebase/auth";
 import * as React from "react";
 import { useLocation, Outlet, Navigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 interface IProtectedRoutesProps {}
 
-const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = (
-  props
-) => {
-  const isAuth: boolean = true;
+const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = () => {
+  const auth = getAuth();
+  const [user, loading] = useAuthState(auth);
   const location = useLocation();
 
-  return isAuth ? (
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return user ? (
     <Outlet />
   ) : (
     <Navigate to="/login" state={{ from: location }} />
